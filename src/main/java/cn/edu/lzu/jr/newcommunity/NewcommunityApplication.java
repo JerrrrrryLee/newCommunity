6e76ad482a3be516c2aac2832046c861d47e93d3
JerrrrrryLee <lijianruilbj@163.com>
2020-05-15 09:17:06 +0800

    first commit

---
+++ src/main/java/cn/edu/lzu/jr/newcommunity/NewcommunityApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.lzu.jr.newcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewcommunityApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/edu/lzu/jr/newcommunity/NewcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.lzu.jr.newcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewcommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
